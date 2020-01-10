a62f22710dca41fabd48a70eb76466fd3a83cc2b
Stefan Niederhauser <ghuder5@gmx.ch>
2020-01-10 14:30:51 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict'
+const imagemin = require('imagemin')
+// const mozjpeg = require('imagemin-mozjpeg')
+const responsive = require('responsive-images-generator2/lib')
+
+module.exports = {
+    imageMinify, imageResponsive
+}
+
+function imageMinify(config) {
+    return function (dest) {
+        const c = Object.assign({}, config)
+        c.destination = dest
+        return input => imagemin([input], c).then(res => ({files: [res[0].destinationPath]}))
+    }
+}
+
+function imageResponsive(configs) {
+    return function (dest) {
+        if (!fs.existsSync(dest)) {
+            fs.mkdirSync(dest, {recursive: true})
+        }
+        const cs = configs.map(c => {
+            const res = Object.assign({}, c)
+            res.rename.dirname = dest
+            return res
+        })
+        return input => responsive.generateResponsiveImages([input], cs).then(res => ({files: res}))
+    }
+}
